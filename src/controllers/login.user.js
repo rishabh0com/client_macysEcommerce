6d4aad2e9cbcf39c1c4a7c0fe6f00b9982658b61
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2024-02-09 21:20:14 +0530

    feat: modify controllers with axios insted of fetch

--- src/controllers/login.user.js
+++ src/controllers/login.user.js
@@ -1,21 +1,28 @@
-
+import axios from "axios";
 export const userLogin = async (details)=>{
     try {
-        const res = await fetch('http://localhost:8080/users/login',{
-            method: 'POST',
-            headers: {
-                "set-cookie": "SameSite=None; Secure",
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify(details),
-        },{
-            credentials : 'include'
+        // const res = await axios.po('http://localhost:8080/users/login',{
+        //     method: 'POST',
+        //     headers: {
+        //         "set-cookie": "SameSite=None; Secure",
+        //         "Content-Type": "application/json"
+        //     },
+        //     body: JSON.stringify(details),
+        // },{
+        //     credentials : 'include'
+        // })
+        // const resData = await res.json()
+        // console.log(resData)
+        console.log("in detail",details)
+        const res = await axios.post('http://localhost:8080/users/login',details,{
+            withCredentials: true
         })
-        const resData = await res.json()
+        const resData = await res;
         console.log(resData)
         return resData;
         
-    } catch (error) {
+    } 
+    catch (error) {
         console.log("login Error",error)
     }
 }
--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,11 +1,27 @@
+import axios from 'axios';
 
-export const findAllProducts = async () => {
+// const url = process.env.API_URL;
+// console.log("url", url)
+//
+// All prduct and product find b         y query 
+export const findAllProducts = async (query) => {
     try {
-        const res = await fetch("http://localhost:8080/products");
-        const resData = await res.json();
-        console.log(resData)
-        return resData;
+        console.log("product controller all")
+        const res = await axios.get(`http://localhost:8080/products?${query}`, { withCredentials: true });
+        console.log("product controller all")
+        return await res.data
     } catch (error) {
         console.log("Error", error);
     }
 };
+
+// this is for product page only
+export const findProductById = async (id) => {
+    try {
+        const res = await axios.get(`http://localhost:8080/products/${id}`, { withCredentials: true })
+        console.log("product controller id is caled")
+        return await res.data;
+    } catch (error) {
+        console.log("Error", error);
+    }
+}
