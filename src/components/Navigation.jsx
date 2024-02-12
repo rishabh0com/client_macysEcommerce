121c9fd7866cb5d1b0861a51a6d529aff24a78d5
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2024-02-12 14:56:14 +0530

    fix: modify Navigation

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -10,53 +10,91 @@ const saleRed = {
   color: "red",
 };
 
+const screen = window.innerWidth;
+
 function Navigation() {
-  return (
+  return (<>
+  {screen > 768 ? (
+        <div className="main">
+        <nav>
+          <div>
+            <Link style={menuStyle} to="/shop?category=women">
+              Women
+            </Link>
+          </div>
+          <div>
+            <Link style={menuStyle} to="/shop?category=men">
+              Men
+            </Link>
+          </div>
+          <div>
+            <Link style={menuStyle} to="/shop?category=kids">
+              Kids & Baby
+            </Link>
+          </div>
+          <div>
+            <Link style={menuStyle} to="/shop?category=homeware">
+              Home
+            </Link>
+          </div>
+          <div>
+            <Link style={menuStyle} to="/shop?subCategory=shoes">
+              Shoes
+            </Link>
+          </div>
+          <div>
+            <Link style={menuStyle} to="/shop?subCategory=handbags">
+              Handbags & Accessories
+            </Link>
+          </div>
+          <div>
+            <Link style={menuStyle} to="/shop?subCategory=jewelry">
+              Jewelry
+            </Link>
+          </div>
+          <div>
+            <Link style={saleRed} to="/shop">
+              Sale
+            </Link>
+          </div>
+        </nav>
+      <hr/>
+      </div>
+  
+  ) : (
     <div className="main">
-      <nav>
-        <div>
-          <Link style={menuStyle} to="/shop?category=women">
-            Women
-          </Link>
-        </div>
-        <div>
-          <Link style={menuStyle} to="/shop?category=men">
-            Men
-          </Link>
-        </div>
-        <div>
-          <Link style={menuStyle} to="/shop?category=kids">
-            Kids & Baby
-          </Link>
-        </div>
-        <div>
-          <Link style={menuStyle} to="/shop?category=homeware">
-            Home
-          </Link>
-        </div>
-        <div>
-          <Link style={menuStyle} to="/shop?subCategory=shoes">
-            Shoes
-          </Link>
-        </div>
-        <div>
-          <Link style={menuStyle} to="/shop?subCategory=handbags">
-            Handbags & Accessories
-          </Link>
-        </div>
-        <div>
-          <Link style={menuStyle} to="/shop?subCategory=jewelry">
-            Jewelry
-          </Link>
-        </div>
-        <div>
-          <Link style={saleRed} to="/shop">
-            Sale
-          </Link>
-        </div>
-      </nav>
-      <hr />
-    </div>
+    <nav className="mobile">
+      <div>
+        <Link style={menuStyle} to="/shop?category=women">
+          Women
+        </Link>
+      </div>
+      <div>
+        <Link style={menuStyle} to="/shop?category=men">
+          Men
+        </Link>
+      </div>
+      <div>
+        <Link style={menuStyle} to="/shop?category=kids">
+          Kids & Baby
+        </Link>
+      </div>
+      <div>
+        <Link style={menuStyle} to="/shop?category=homeware">
+          Home
+        </Link>
+      </div>
+      <div>
+        <Link style={saleRed} to="/shop">
+          Sale
+        </Link>
+      </div>
+    </nav>
+    {screen > 768 && <hr/>}
+  </div>
+
+  )}
+  </>
   );
 }
 
