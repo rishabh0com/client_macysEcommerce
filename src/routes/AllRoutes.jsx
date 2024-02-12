e513bda52cfb8c92c81c5d4c3ce0d92b2c32c047
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2024-02-12 15:02:21 +0530

    feat: added new route

--- src/routes/AllRoutes.jsx
+++ src/routes/AllRoutes.jsx
@@ -6,16 +6,19 @@ import Login from '../pages/Login.jsx'
 import AccountPage from '../pages/Account.jsx'
 import ProductPage from '../pages/Product.jsx'
 import Products from '../pages/Products.jsx'
+import Cart from '../pages/Cart.jsx'
 
 
 const AllRoutes = () => {
+  const access = localStorage.getItem("access");
   return (
     <div>
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="signup" element={<SignUp />} />
             <Route path="/login" element={<Login />} />
-            <Route path="/account" element={<AccountPage/>} />
+            {access && (<Route path="/account" element={<AccountPage/>}/>)}
+            <Route path="/shop/cart" element={<Cart/>} />
             <Route path="/shop" element={<Products/>} />
             <Route path="/shop/:id" element={<ProductPage/>} />
         </Routes>
