355b2987fbcfc57bb2022767a4ef64453b2bb2cd
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2024-02-12 15:05:44 +0530

    fix: some lines

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
-import { useState } from "react";
-import "./App.css";
+import { useState , useContext, useEffect} from "react";
+import styles from "./App.module.css";
 import AllRoutes from "./routes/AllRoutes.jsx";
-import { UserProvider } from "./contexts/detail.user.jsx";
+import { UserContext, UserProvider } from "./contexts/detail.user.jsx";
+import Loader from "./components/subComponents/Loader.jsx";
+import { useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Footer from "./components/Footer.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
-
+  const {loading,setLoading}  = useContext(UserContext);
+  const location = useLocation();
+  console.log("in app location",location.pathname);
+  useEffect(() => {
+    setLoading(true)
+    setTimeout(() => {
+      setLoading(false)
+    }, 250);
+  }, [location]);
   return (
-    <UserProvider>
-      <AllRoutes />
-    </UserProvider>
+    <div className={styles.app}>
+    <Navbar/>
+          {loading ? <Loader/> : <AllRoutes/>}
+     <Footer/>
+
+    </div>
   );
 }
 
