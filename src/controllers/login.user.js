be2bfafe4aec57d29cdf908651a4e8763c7f53d6
Rishabh Tripathi <rishabhtripathi587@gmail.com>
2024-02-21 08:45:14 +0530

    fix: set headers for authorization

--- src/controllers/login.user.js
+++ src/controllers/login.user.js
@@ -1,39 +1,41 @@
 import axios from "axios";
 const api = import.meta.env.VITE_API_URL;
 
+export const tokens = JSON.parse(localStorage.getItem("tokens"));
 
 export const userLogin = async (details) => {
-    // const {setUserDetail } = useContext(UserContext);
-    
-    try {
-        console.log("in detail", details)
-        const res = await axios.post(`${api}/users/login`, details, {
-            withCredentials: true
-        })
-        const resData = await res;
-        console.log("in login controller", resData)
-        if (resData.data.success) {
-            const { firstName, lastName, email, _id } = resData.data.data;
-            console.log(resData.data.data)
-            localStorage.setItem("userInfo", JSON.stringify({ firstName, lastName, email, userId: _id }));
-            localStorage.setItem("access", true);
-        }
+  // const {setUserDetail } = useContext(UserContext);
 
-        return resData.data;
-
+  try {
+    console.log("in detail", details);
+    const res = await axios.post(`${api}/users/login`, details, {
+      withCredentials: true,
+    });
+    const resData = await res;
+    console.log("in login controller", resData);
+    if (resData.data.success) {
+      const { firstName, lastName, email, _id } = resData.data.data.user;
+      //   console.log(resData.data.data);
+      console.log("Log In Data : ", resData.data.data);
+      const { accessToken, refreshToken } = resData.data.data;
+      localStorage.setItem(
+        "tokens",
+        JSON.stringify({ accessToken, refreshToken })
+      );
+      localStorage.setItem(
+        "userInfo",
+        JSON.stringify({ firstName, lastName, email, userId: _id })
+      );
+      localStorage.setItem("access", true);
     }
-    catch (error) {
-        
-        if (error.response) {
-            return error.response.data;
-        } else {
-            console.log("login Error", error)
 
-        }
-    }finally{
-        
-
+    return resData.data;
+  } catch (error) {
+    if (error.response) {
+      return error.response.data;
+    } else {
+      console.log("login Error", error);
     }
-}
-
-
+  } finally {
+  }
+};
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,42 +1,48 @@
-import React, { useState,useContext } from 'react';
-import styles from '../styles/account.module.css';
-import {UserContext} from "../contexts/detail.user.jsx";
-import axios from 'axios';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useContext } from "react";
+import styles from "../styles/account.module.css";
+import { UserContext } from "../contexts/detail.user.jsx";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { tokens } from "../controllers/login.user.js";
 const api = import.meta.env.VITE_API_URL;
 
-
 const AccountPage = () => {
-  const {userDetail} = useContext(UserContext);
-  const {firstName,lastName,email} = JSON.parse(localStorage.getItem("userInfo"));
+  const { userDetail } = useContext(UserContext);
+  const { firstName, lastName, email } = JSON.parse(
+    localStorage.getItem("userInfo")
+  );
   const navigate = useNavigate();
-  
-  const handleClick = async () => {
-    try {
-     localStorage.removeItem("userInfo");
-    localStorage.removeItem("access");
-    const res = await axios.get(`${api}/users/logout`,{withCredentials:true});
-    const resData = await res;
-    resData && alert(resData.data.message);
-    // window.location.href = "/login";
-    navigate("/login");
 
+  const handleClick = async () => {
+    const { accessToken, refreshToken } = tokens;
+    try {
+      localStorage.clear();
+      const res = await axios.get(`${api}/users/logout`, {
+        withCredentials: true,
+        headers: { Authorization: `${accessToken},${refreshToken}` },
+      });
+      const resData = await res;
+      resData && alert(resData.data.message);
+      // window.location.href = "/login";
+      navigate("/login");
     } catch (error) {
-      console.log("logout error",error)
-      
+      console.log("logout error", error);
+      navigate("/login");
     }
   };
 
   return (
     <>
-    <div className={styles.accountPage}>
-      <div className={styles.userInfo}>
-        <h2 className={styles.name}>{`${firstName}  ${lastName}`}</h2>
-        <p>Email: {email}</p>
-        <button className={styles.logout} onClick={handleClick}>Logout</button>
+      <div className={styles.accountPage}>
+        <div className={styles.userInfo}>
+          <h2 className={styles.name}>{`${firstName}  ${lastName}`}</h2>
+          <p>Email: {email}</p>
+          <button className={styles.logout} onClick={handleClick}>
+            Logout
+          </button>
+        </div>
+        {/* Add additional features, such as editing user info or settings */}
       </div>
-      {/* Add additional features, such as editing user info or settings */}
-    </div>
     </>
   );
 };
